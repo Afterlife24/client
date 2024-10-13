56b94e72147d10d8ea3aac1ed00d19ef70b140e5
Afterlife24 <afterlife668@gmail.com>
2024-10-13 08:58:39 +0530

    tapandcollect_1

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ const { getFirestore } = require('firebase/firestore');
 
 
 // scanmeprod-productionFIRESTORE
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBwMWR-QyG3LnkeOmI5_QBbI_FoL_skIHc",
-//   authDomain: "scanmeprod.firebaseapp.com",
-//   projectId: "scanmeprod",
-//   storageBucket: "scanmeprod.appspot.com",
-//   messagingSenderId: "3426314793",
-//   appId: "1:3426314793:web:ec931d424b430196ae4a38",
-//   measurementId: "G-K7TP3HFDM0"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBwMWR-QyG3LnkeOmI5_QBbI_FoL_skIHc",
+  authDomain: "scanmeprod.firebaseapp.com",
+  projectId: "scanmeprod",
+  storageBucket: "scanmeprod.appspot.com",
+  messagingSenderId: "3426314793",
+  appId: "1:3426314793:web:ec931d424b430196ae4a38",
+  measurementId: "G-K7TP3HFDM0"
+};
 
 
 // const firebaseConfig = {
@@ -37,18 +37,6 @@ const { getFirestore } = require('firebase/firestore');
 //   appId: "1:948074886578:web:80a62ba4fb8f3d24a3a0ab",
 //   measurementId: "G-HBMXG6WXR0"
 // };
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAdPjzBE2EjsCaTP5aBQ9w1-_TKL2zttw8",
-    authDomain: "tapandcollect-abe43.firebaseapp.com",
-    projectId: "tapandcollect-abe43",
-    storageBucket: "tapandcollect-abe43.appspot.com",
-    messagingSenderId: "835659748911",
-    appId: "1:835659748911:web:effae864ee493a85b798da",
-    measurementId: "G-DE5GCGGHRJ"
-  };
 
   
   const app = initializeApp(firebaseConfig);
